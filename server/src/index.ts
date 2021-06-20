dfdfad9c94e00edfb450a7fe226a58c7bf63f335
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-20 09:47:56 +0545

    Server Setup

---
+++ server/src/index.ts
@@ -0,0 +1,61 @@
+import "dotenv/config";
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./utils/UserResolver";
+import { createConnection } from "typeorm";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+
+import { User } from "./entity/User";
+import { createAccessToken, createRefreshToken } from "./utils/auth";
+import { sendRefreshToken } from "./utils/sendRefreshToken";
+
+(async () => {
+  const app = express();
+  await createConnection();
+
+  app.use(cookieParser());
+
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    if (user.tokenVersion !== payload.tokenVersion) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ res, req }) => ({ res, req }),
+  });
+
+  app.listen(4000, () => {
+    console.log("Express server started!");
+  });
+
+  apolloServer.applyMiddleware({ app });
+})();
---
+++ server/src/utils/UserResolver.ts
@@ -0,0 +1,97 @@
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware,
+  Int,
+} from "type-graphql";
+import { hash, compare } from "bcryptjs";
+
+import { User } from "../entity/User";
+import { MyContext } from "./MyContext";
+import { createRefreshToken, createAccessToken } from "./auth";
+import { isAuth } from "./IsAuth";
+import { sendRefreshToken } from "./sendRefreshToken";
+import { getConnection } from "typeorm";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => String)
+  hello() {
+    return "hi";
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Mutation(() => Boolean)
+  async revokeRefreshTokensForUser(@Arg("userId", () => Int) userId: number) {
+    await getConnection()
+      .getRepository(User)
+      .increment({ id: userId }, "tokenVersion", 1);
+
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+    return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("Could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Wrong password");
+    }
+
+    sendRefreshToken(res, createRefreshToken(user));
+
+    return {
+      accessToken: createAccessToken(user),
+    };
+  }
+}
